81a075d6f311f30e5fbc068e500e3f0452c00c46
a-slide <adrien.leger@gmail.com>
2014-08-06 19:01:00 +0200

    Add reference source and star to adapt ContaVect

---
+++ src/CV_Reference.py
@@ -0,0 +1,158 @@
+#~~~~~~~GLOBAL IMPORTS~~~~~~~#
+
+# Standard library packages import
+import gzip
+from sys import stdout
+from os import path
+
+# Third party packages import
+from Bio import SeqIO
+
+# Local Package import
+from Utilities import file_basename
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+class Reference(object):
+    """
+    @class  Reference
+    @brief  Object oriented class containing informations of reference
+    """
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+    #~~~~~~~CLASS FIELDS~~~~~~~#
+
+    Instances = [] # Class field used for instance tracking
+    id_count = 0
+
+    #~~~~~~~CLASS METHODS~~~~~~~#
+    
+    @ classmethod
+    def next_id (self):
+        cur_id = self.id_count
+        self.id_count +=1
+        return cur_id
+    
+    @ classmethod
+    def countInstances (self):
+        return len(self.Instances)
+        
+    @ classmethod
+    def allSeqList (self):
+        if self.Instances:
+            all_seq_list=[]
+            for ref in self.Instances:
+                all_seq_list.extend(ref.seq_list)
+            return all_seq_list
+        else:
+            return []
+
+    @ classmethod
+    def seqOrigin (self, seqname):
+        for ref in self.Instances:
+            if seqname in ref.seq_list:
+                return ref.name
+        return None
+    
+    @ classmethod
+    def allName (self):
+        return [ref.name for ref in self.Instances]
+    
+    @ classmethod
+    def allFasta (self):
+        return [ref.fasta_path for ref in self.Instances]
+    
+    @ classmethod
+    def getInstances (self):
+        return self.Instances
+    
+    @ classmethod
+    def printInstances (self):
+        for ref in self.Instances:
+            print (repr(ref))
+    
+    @ classmethod
+    def resetInstances (self):
+        print "Clearing Reference instances list"
+        self.Instances = []
+        self.id_count = 0
+        
+    #~~~~~~~FONDAMENTAL METHODS~~~~~~~#
+
+    def __init__(self,
+            fasta_path,
+            mk_sam=False,
+            mk_covgraph=False,
+            mk_bedgraph=False,
+            mk_vcf=False):
+        """
+        """
+        self.name = file_basename(fasta_path)
+        print ("Creating reference object {}".format(self.name))
+        self.id = self.next_id()
+        self.fasta_path = path.abspath(fasta_path)
+        self.seq_list = self._fasta_reader()
+        #self.bam = []
+        self.mk_sam = mk_sam
+        self.mk_covgraph = mk_covgraph
+        self.mk_bedgraph = mk_bedgraph
+        self.mk_vcf = mk_vcf
+        
+        # Add the instance to the class instance tracking list
+        self.Instances.append(self)
+
+    def __repr__(self):
+        msg = "REF {}".format(self.id)
+        msg+= "\tName: {}\n".format(self.name)
+        msg+= "\tFasta_path: {}\n".format(self.fasta_path)
+        msg+= "\tSequence list:  {}\n".format("  ".join(self.seq_list))
+        msg+= "\tRequired output:"
+        
+        if self.mk_sam:
+            msg+= "  Sam file"
+        if self.mk_covgraph:
+            msg += "  Coverage graph"
+        if self.mk_bedgraph:
+            msg += "  Bedgraph file"
+        if self.mk_vcf:
+            msg += "  VCF file"
+        msg+="\n"
+        return (msg)
+
+    def __str__(self):
+        return "<Instance of {} from {} >\n".format(self.__class__.__name__, self.__module__)
+
+    #~~~~~~~FONDAMENTAL METHODS~~~~~~~#
+    
+    def _fasta_reader(self):
+        """
+        Read seq names a fasta file and verify the absence of duplicates
+        """
+        try:
+            if self.fasta_path[-2:].lower() == "gz":
+                fp = gzip.open(self.fasta_path,"rb")
+            else:
+                fp = open(self.fasta_path,"rb")
+                
+        except Exception as E:
+            fp.close()
+            raise Exception (E.message+"Can not create a list of sequence from{}".format(self.name))
+        
+        seq_list=[]
+        for seq in SeqIO.parse(fp, "fasta"):
+            # verify the absence of the sequence name in the current list and in other ref seq_list
+            if seq.id in seq_list:
+                raise Exception ("{} is duplicateda in the current reference\n".format(seq.id, self.name))
+            if seq.id in self.allSeqList():
+                raise Exception ("{} is duplicated in another reference\n".format(seq.id))
+            else:
+                seq_list.append(seq.id)
+
+            stdout.write("*")
+            stdout.flush()
+        
+        print ("")
+        fp.close()
+        
+        return seq_list
+
+
--- src/ContaVect.py
+++ src/ContaVect.py
@@ -1,13 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 #~~~~~~~GLOBAL IMPORTS~~~~~~~#
 
 # Standard library packages import
 from os import path
-from tempfile import mkdtemp
-from shutil import rmtree
-from time import time
+#from tempfile import mkdtemp
+#from shutil import rmtree
+from time import time, sleep
+import ConfigParser
+from sys import argv
 
 # Local Package import
-from Utilities import mkdir, file_basename, file_name, expand_filelist
+from Utilities import mkdir, file_basename, file_name, expand_file
 from Blast import Blastn
 import RefMasker
 from FastqFT.FastqFilterPP import FastqFilterPP
@@ -16,167 +22,168 @@ from FastqFT.AdapterTrimmer import AdapterTrimmer
 from Ssw import ssw_wrap
 from FastaReader2 import FastaReader2 as FastaReader
 from Bwa import Mem
+from CV_Reference import Reference
 
 #~~~~~~~MAIN FUNCTION~~~~~~~#
 
-def maindef(ref_list, R1, R2, outdir="./", outprefix="out"):
+def main(conf):
 
     stime = time()
 
-    # Reference Masking
-    ref_masking = True
-    blastn_opt=None
-    mkblastdb_opt=None
-
-    # Fastq Filtering
-    quality_filtering = True
-    min_qual = 30
-    input_qual = "fastq-sanger"
-    adapter_trimming = True
-    adapter_path = "/media/analyse/Pharmaco_AAV/documents_pharmaco_AAV/Programming/Python/ContaVect/test/adapter.fa"
-    # ... Other parameters not available for now
-
-    # Alignment
-    bwa_index = None
-    mem_opt = None
-    index_opt = None
-
     # Create a main directory for file created during the programm
-    main_dir = path.abspath(outdir)+"/"
+    main_dir = path.abspath(conf.get("General", "outdir"))+"/"
     mkdir(main_dir)
-
-    print ("\n##### EXPAND AND REGROUP REFERENCES #####\n")
+    
+    print ("\n##### EXPAND AND PARSE REFERENCES #####\n")
     # Expand the reference sequence to avoid multiple decompression during Program execution
+    
     ref_dir = path.join(main_dir+"references/")
     mkdir(ref_dir)
-    ref_list = expand_filelist (ref_list, outdir=ref_dir, copy_ungz=True)
-    #~print "\nReference list:\n" + "\n".join([ref for ref in ref_list]) + "\n"
-
+    extract_ref(conf, ref_dir) # Reference object are instancied and accessible through the class methods 
+    
     # Reference Masking
-    if ref_masking:
+    if conf.get("Ref_Masking", "ref_masking"):
         print ("\n##### REFERENCE HOMOLOGIES MASKING #####\n")
         db_dir = path.join(main_dir, "blast_db/")
         mkdir (db_dir)
-        ref_list = iterative_masker(ref_list, db_dir, ref_dir, blastn_opt, mkblastdb_opt)
-
-    print "\nReference list:\n" + "\n".join([ref for ref in ref_list]) + "\n"
-
+        ref_list = iterative_masker(conf, db_dir, ref_dir)
+    
     # Fastq Filtering
-    if quality_filtering or adapter_trimming:
+    if conf.get("Fastq_Filtering", "quality_filtering") or conf.get("Fastq_Filtering", "adapter_trimming"):
         print ("\n##### FASTQ FILTERING #####\n")
         fastq_dir = path.join(main_dir+"fastq/")
         mkdir(fastq_dir)
-        R1, R2 = fastq_filter(R1, R2, fastq_dir, quality_filtering, min_qual, input_qual, adapter_trimming, adapter_path, maxqueue)
-
+        R1, R2 = fastq_filter(conf, fastq_dir)
+    else:
+        R1, R2 = conf.get("Fastq", "R1"), conf.get("Fastq", "R2") 
+    
     print "R1 = " + R1
     print "R2 = " + R2
 
-    # BWA alignment
-    print ("\n##### READ REFERENCES AND ALIGN WITH BWA #####\n")
+    ## BWA alignment
+    #print ("\n##### READ REFERENCES AND ALIGN WITH BWA #####\n")
 
-    # Create a folder for aligned read storage
-    align_dir = path.join(main_dir+"bwa_align/")
-    mkdir(align_dir)
+    ## Create a folder for aligned read storage
+    #align_dir = path.join(main_dir+"bwa_align/")
+    #mkdir(align_dir)
 
-    if ref_masking or not bwa_index:
-        # Read fasta ref to map seq names and merge then together in a single fasta file
-        reader = FastaReader(ref_list, write_merge=True, output=ref_dir+"merged.fa", write_summary=True)
+    #if ref_masking or not bwa_index:
+        ## Read fasta ref to map seq names and merge then together in a single fasta file
+        #reader = FastaReader(ref_list, write_merge=True, output=ref_dir+"merged.fa", write_summary=True)
 
-        # Create a folder for index storage
-        index_dir = path.join(main_dir+"bwa_index/")
-        mkdir(index_dir)
+        ## Create a folder for index storage
+        #index_dir = path.join(main_dir+"bwa_index/")
+        #mkdir(index_dir)
 
-        # Index and Align with Bwa Mem
-        Mem.align (R1,R2, ref_fasta=reader.getMergeRef(),
-                   align_opt=mem_opt, align_outdir=align_dir, align_outname=outprefix+".sam",
-                   index_opt=index_opt, index_outdir=index_dir, index_outname = "merged.fa")
+        ## Index and Align with Bwa Mem
+        #Mem.align (R1,R2, ref_fasta=reader.getMergeRef(),
+                   #align_opt=mem_opt, align_outdir=align_dir, align_outname=outprefix+".sam",
+                   #index_opt=index_opt, index_outdir=index_dir, index_outname = "merged.fa")
 
-    else:
-        # Read fasta ref to map seq names
-        reader = FastaReader(ref_list, write_summary=True)
+    #else:
+        ## Read fasta ref to map seq names
+        #reader = FastaReader(ref_list, write_summary=True)
 
-        # Align with Bwa Mem against
-        Mem.align (R1,R2, ref_index=bwa_index,
-                   align_opt=mem_opt, align_outdir=align_dir, align_outname=outprefix+".sam")
+        ## Align with Bwa Mem against
+        #Mem.align (R1,R2, ref_index=bwa_index,
+                   #align_opt=mem_opt, align_outdir=align_dir, align_outname=outprefix+".sam")
 
-    print ("\n##### DONE #####\n")
-    print ("Total execution time = {}s".format(round(time()-stime, 2)))
+    #print ("\n##### DONE #####\n")
+    #print ("Total execution time = {}s".format(round(time()-stime, 2)))
 
 #~~~~~~~HELPER FUNCTIONS~~~~~~~#
 
-def iterative_masker (
-    ref_list,
-    db_dir="./",
-    ref_dir="./",
-    align_opt=None,
-    db_opt=None):
-    new_ref_list = []
+def extract_ref(conf, refdir="./"):
+    """
+    Import and expand fasta references and associated flags in a Reference object
+    """
+    i = 1
+    while True:
+        # Iterate over Ref in the config file until no more reference is found
+        ref_id = "Ref"+str(i)
+        
+        if not conf.has_section(ref_id):
+            break
+        try:
+            fasta = expand_file(conf.get(ref_id, "fasta"), outdir=refdir, copy_ungz=True)
+            sam = conf.getboolean (ref_id, "sam")
+            covgraph = conf.getboolean (ref_id, "covgraph")
+            bedgraph = conf.getboolean (ref_id, "bedgraph")
+            vcf = conf.getboolean (ref_id, "vcf")            
+        except ValueError:
+            sleep (2)
+            print ("ERROR INVALID VALUES, SKIPPING THE REFERENCE NUMBER "+str(i))
+        else:
+            a = Reference(fasta, sam, covgraph, bedgraph, vcf)
+            print (repr(a))
+            i+=1
 
-    while (len(ref_list)>=2):
+def iterative_masker (conf, db_dir="./", ref_dir="./"):
+    
+    # Iterate over index in Reference.instances staring by the last one until the 2nd one
+    for i in range(Reference.countInstances()-1, 0, -1):
         # Extract subject and query_list from ref_list
-        subject = ref_list [-1]
-        query_list = ref_list [0:-1]
-        subject_name = file_basename(subject)
-        print ("\n# PROCESSING REFERENCE {} #\n".format(subject_name ))
+        subject = Reference.Instances[i]
+        query_list = Reference.Instances[0:i]
+        print ("\n# PROCESSING REFERENCE {} #\n".format(subject.name))
 
         # Perform a blast of query list against subject
         hit_list = Blastn.align (
-            query_list,
-            subject_fasta=subject,
-            evalue=0.5,
-            align_opt=None,
-            db_opt=None,
+            [ref.fasta_path for ref in query_list],
+            subject_fasta=subject.fasta_path,
+            align_opt=conf.get("Ref_Masking", "blastn_opt"),
+            db_opt=conf.get("Ref_Masking", "mkblastdb_opt"),
             db_outdir = db_dir,
-            db_outname = subject_name)
+            db_outname = subject.name)
 
-        # Masking hits in suject fasta
-        new_ref_list.append (RefMasker.mask (
-            subject,
+        # Masking hits in suject fasta if hits in hit_list
+        subject.fasta_path = RefMasker.mask (
+            subject.fasta_path,
             hit_list,
             ref_outdir=ref_dir,
-            ref_outname="masked_{}.fa".format(subject_name),
-            compress_ouput=False ))
+            ref_outname="masked_{}.fa".format(subject.name),
+            compress_ouput=False )
 
-        # Remove the last last element of the list
-        ref_list.pop()
-
-    # Adding the last reference that was not poped out of the list
-    new_ref_list.extend(ref_list)
-    # Reverse the list to be in the original order
-    new_ref_list.reverse()
-    return new_ref_list
-
-
-def fastq_filter (
-    R1, R2,
-    outdir="./",
-    quality_filtering=False,
-    min_qual=0,
-    input_qual="fastq-sanger",
-    fastq_trimming=False,
-    adapter_path=""):
+def fastq_filter (conf, outdir="./"):
 
     # Define a quality filter object
-    if quality_filtering:
-        qFilter = QualityFilter (min_qual)
-    else:
-        qFilter = None
+    qFilter = None
+    if conf.get("Fastq_Filtering", "quality_filtering"):
+        qFilter = QualityFilter (conf.getfloat("Fastq_Filtering", "min_qual"))
 
     # Define a adapter trimmer object
-    if fastq_trimming:
-        sswAligner = ssw_wrap.Aligner() # Instanciate with a default score matrix
-        trimmer = AdapterTrimmer(sswAligner, adapter_path) # Instanciate with a default score filters
-    else:
-        trimmer = None
+    trimmer = None
+    if conf.get("Fastq_Filtering", "adapter_trimming"):
+        sswAligner = ssw_wrap.Aligner(
+            match=conf.getint("Fastq_Filtering", "ssw_match"),
+            mismatch=conf.getint("Fastq_Filtering", "ssw_mismatch"),
+            gap_open=conf.getint("Fastq_Filtering", "ssw_gap_open"),
+            gap_extend=conf.getint("Fastq_Filtering", "ssw_gap_extend"))
+        trimmer = AdapterTrimmer(
+            aligner=sswAligner,
+            adapter_path=conf.get("Fastq_Filtering", "adapter_path"),
+            min_read_len=conf.getfloat("Fastq_Filtering", "min_read_len"),
+            min_match_len=conf.getfloat("Fastq_Filtering", "min_match_len"),
+            min_match_score=conf.getfloat("Fastq_Filtering", "min_match_score"))
 
     # Filter fastq for quality and adapter with FastqFilterPP
-    fFilter = FastqFilterPP (R1, R2,
+    fFilter = FastqFilterPP (
+        R1=conf.get("Fastq", "R1"),
+        R2=conf.get("Fastq", "R1"),
         quality_filter=qFilter,
         adapter_trimmer=trimmer,
         outdir=outdir,
-        input_qual=input_qual,
-        maxqueue=maxqueue,
+        input_qual=conf.get("Fastq_Filtering", "input_qual"),
         compress_output=False)
     
     print (repr(fFilter))
-    return (fFilter.R1_out, fFilter.R2_out)
+    return fFilter.getTrimmed()
+
+
+#~~~~~~~TOP LEVEL INSTRUCTIONS~~~~~~~#
+if __name__ == '__main__':
+    
+    conf = ConfigParser.RawConfigParser(allow_no_value=True)
+    conf.read(argv[1])
+    main(conf)
+
